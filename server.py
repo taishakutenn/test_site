69be5d6d8e700d38bf058bafc8472f05eeaf2633
taishakutenn <sunlesslove128@gmail.com>
2025-03-21 16:27:42 +0500

    second commit

--- server.py
+++ server.py
@@ -1,18 +1,15 @@
-from flask import Flask, render_template, url_for
+import os
+
+from flask import Flask
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
-
-def main():
-    app.run()
 
 
 @app.route("/")
-@app.route('/index')
 def index():
-    return "Мой сайт!"
+    return "Привет от приложения Flask"
 
 
 if __name__ == '__main__':
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
